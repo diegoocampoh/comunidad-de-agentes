1970bf365bdd2c2e0957f7dcbdb7ad03b791d382
carlux <carlux@617b6c5c-cba9-11de-9a52-05a2856a7527>
2009-11-12 16:44:46 +0000

    ll

---
+++ trunk/comunidadAgentes/src/comunidadagentes/DocumentoInterfazVocabulario.java
@@ -0,0 +1,9 @@
+package comunidadagentes;
+public interface DocumentoInterfazVocabulario {
+	String ONTOLOGY_NAME = "DocumentoOntologia";
+        String PROVEEDOR = "Provee";
+	String DOCUMENTO = "Documento";
+	String CONTENIDO = "Contenido";
+	String TITULO = "Titulo";
+	String AUTOR= "Autor";
+}
