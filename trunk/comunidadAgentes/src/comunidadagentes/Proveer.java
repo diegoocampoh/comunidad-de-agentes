c4ddba33d6cfbf0ecc861b0d7e1ff36945610a4c
eldiegolp <eldiegolp@617b6c5c-cba9-11de-9a52-05a2856a7527>
2009-11-12 19:15:47 +0000

    ahi va ese resultado luchovidalin

--- trunk/comunidadAgentes/src/comunidadagentes/Proveer.java
+++ trunk/comunidadAgentes/src/comunidadagentes/Proveer.java
@@ -5,23 +5,33 @@
 
 package comunidadagentes;
 
+import jade.content.ContentElement;
 import jade.content.Predicate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
- *
+ * Clase utilizada como predicado.
+ * Se utiliza para el comienzo del protocolo Contract Net.
+ * El que pide las ofertas utiliza esta clase, pasando las keywords de los
+ * documentos que está buscando por medio de este objeto.
+ * Los proveedores se basan en estas keywords y setean
  * @author Diego
  */
-public class Proveer implements Predicate {
+public class Proveer implements Predicate, ContentElement {
 
     private List<String> keywords = new ArrayList<String>();
-    private int puntaje;
+   
 
     public Proveer()
     {
 
+
+    }
+    public Proveer(List<String> keywords)
+    {
+        
+        
     }
 
     public List<String> getKeywords() {
@@ -31,19 +41,5 @@ public class Proveer implements Predicate {
     public void setKeywords(List<String> keywords) {
         this.keywords = keywords;
     }
-
-    public int getPuntaje() {
-        return puntaje;
-    }
-
-    public void setPuntaje(int puntaje) {
-        this.puntaje = puntaje;
-    }
-
-   
-
-
-
-  
 
 }
