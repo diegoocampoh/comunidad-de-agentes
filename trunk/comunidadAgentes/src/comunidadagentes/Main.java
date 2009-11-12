24271bc7ab007125ea5ddce38544de8f214014b5
carlux <carlux@617b6c5c-cba9-11de-9a52-05a2856a7527>
2009-11-12 16:44:20 +0000

    ll

--- trunk/comunidadAgentes/src/comunidadagentes/Main.java
+++ trunk/comunidadAgentes/src/comunidadagentes/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package comunidadagentes;
 import jade.core.Agent;
 import jade.domain.DFService;
